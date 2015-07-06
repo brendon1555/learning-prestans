01abd81e3469c9b4d6ca63bff05e0e4b1381dcc7
Brendon Lees <brendon1555@gmail.com>
2015-07-06 15:53:16 +1000

    Added login/logout link to template

--- app/opeth/page/handlers.py
+++ app/opeth/page/handlers.py
@@ -18,7 +18,20 @@ class Base(webapp2.RequestHandler):
 #webapp2
 class MainHandler(Base):
     def get(self):
-        self.render_template("index")
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext
+        }
+        self.render_template("index", template_values)
+
 
 class StaticHandler(Base):
     def get(self):
